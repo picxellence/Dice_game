9e94b6dc0818cd585305a54935d0883d11f34230
picxellence <ujjwalsrivastava1710@gmail.com>
2024-08-30 01:01:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 var randomImage1 = "dice" + randomNumber1 + ".png";
-var randomImageSource1 = "images/" + randomImage1;
+var randomImageSource1 = randomImage1;
 
 var randomImage2 = "dice" + randomNumber2 + ".png";
-var randomImageSource2 = "images/" + randomImage2;
+var randomImageSource2 = randomImage2;
 
 
 document.querySelector(".img1").setAttribute("src" , randomImageSource1);
